b6df4a69138e6e801b36776baa7500077c721085
Kriss322 <krist2397@gmail.com>
2019-02-10 18:22:47 +0100

    Initialized the project

---
+++ src/main/java/com/tribe/Tribes/TribesApplication.java
@@ -0,0 +1,14 @@
+package com.tribe.Tribes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TribesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TribesApplication.class, args);
+	}
+
+}
+
