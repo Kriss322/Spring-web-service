85b708ce6b2236e495075f7e7c37e26bf2360ac3
Kriss322 <krist2397@gmail.com>
2019-03-12 17:20:38 +0100

    monir changes to ui

--- tribes.ui/client/src/app/app.module.ts
+++ tribes.ui/client/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import {MaterialModule} from './material';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
