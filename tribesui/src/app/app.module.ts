1b216c566620af5c9ebd38ff64311e24d1315bd7
Kriss322 <krist2397@gmail.com>
2019-03-20 16:22:53 +0100

    changes to the ui

---
+++ tribesui/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MatMenuModule } from '@angular/material/menu';
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidenavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatSidenavModule,MatListModule, MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
